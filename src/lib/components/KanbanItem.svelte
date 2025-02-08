86dc95fa6f69b90d0c0ad1e05532181bc66e4bac
bangueco <121280104+bangueco@users.noreply.github.com>
2025-02-08 21:29:51 +0800

    chore: add delete functionality for kanban itemsc

--- src/lib/components/KanbanItem.svelte
+++ src/lib/components/KanbanItem.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
     import type { Items } from "../../types";
     import { dragItem } from "../actions.svelte";
+    import storage from "../storage";
+    import { kanbanItemsStore } from "../stores.svelte";
 
     let { id, item_name, status }: Items = $props()
+
+    const onPressDelete = (id: number) => {
+        kanbanItemsStore.update((items) => {
+            const newItems = items.filter(item => item.id !== id)
+
+            return newItems
+        })
+
+        storage.setLocalStorageItem("kanban-items", JSON.stringify($kanbanItemsStore))
+    }
 </script>
 
-<p class="border p-3 text-center cursor-pointer" use:dragItem data-id={id} data-label={status}>{item_name}</p>
+<div class="border p-3 cursor-pointer relative flex justify-between" use:dragItem data-id={id} data-label={status}>
+    <p>{item_name}</p>
+    <div>
+        <button class="text-red-700" onclick={() => onPressDelete(id)}>Delete</button>
+    </div>
+</div>
